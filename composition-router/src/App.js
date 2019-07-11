eed0614524c0c79b2420be8064065115af1f5c38
unknown <lydt@htactive.com>
2019-07-11 13:44:56 +0700

    split getData in App.js

--- composition-router/src/App.js
+++ composition-router/src/App.js
@@ -1,17 +1,68 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.css";
+import axios from "axios";
 import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
-import routers from "./router/router";
 
-function App() {
-  const routerList = routers.map(({ path, main, exact }, index) => (
-    <Route path={path} component={main} exact={exact} key={index} />
-  ));
-  return (
-    <Router>
-      <Switch>{routerList}</Switch>
-    </Router>
-  );
+import User from "./router/page/user";
+import Contact from "./router/page/contact";
+import Home from "./router/page/home";
+import Notfound from "./router/page/notfound";
+import ProductDetail from "./router/page/productDetail";
+import IsLoading from "./router/components/isLoading";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: []
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get("http://5d19c3a8b3b6a100148d22b1.mockapi.io/startWithReact")
+      .then(res => {
+        if (res.status === 200) {
+          this.setState({
+            products: res.data,
+            isLoaded: true
+          });
+        }
+      });
+  }
+
+  render() {
+    const { products, isLoaded } = this.state;
+    return (
+      <>
+        {isLoaded ? (
+          <Router>
+            <Switch>
+              <Route
+                exact
+                path="/"
+                component={() => <Home products={products} />}
+              />
+              <Route path="/users" component={User} />
+              <Route path="/contact" component={Contact} />
+              <Route
+                path="/product/:id"
+                component={({ match }) => (
+                  <ProductDetail
+                    products={products}
+                    match={match}
+                  />
+                )}
+              />
+              <Route component={Notfound} />
+            </Switch>
+          </Router>
+        ) : (
+          <IsLoading />
+        )}
+      </>
+    );
+  }
 }
 
 export default App;
--- composition-router/src/router/page/productDetail.js
+++ composition-router/src/router/page/productDetail.js
@@ -1,60 +1,31 @@
-import axios from "axios";
 import Header from "../components/Header";
-import React, { Component } from "react";
-import IsLoading from "../components/isLoading";
+import React from "react";
 
-export default class productDetail extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      product: {},
-      isLoaded: false
-    };
-  }
-  getProduct = id => {
-    axios
-      .get(`http://5d19c3a8b3b6a100148d22b1.mockapi.io/startWithReact/${id}`)
-      .then(res =>
-        this.setState({
-          product: res.data,
-          isLoaded: true
-        })
-      );
-  };
-  async componentDidMount() {
-    const { id } = this.props.match.params;
-    this.getProduct(id);
-  }
-
-  render() {
-    const {isLoaded, product} = this.state;
-    const { title, img, description } = product;
-    return (
-      <>
-        {isLoaded ? (
-          <div>
-            <Header />
-            <div className="container">
-              <h4>PRODUCT DETAIL</h4>
-              <div className="row">
-                <div className="col-sm-5">
-                  <img src={img} alt="Img" width="100%" />
-                </div>
-                <div className="col-sm-7">
-                  <p>
-                    <b>Name:</b> {title}
-                  </p>
-                  <p>
-                    <b>Description:</b> {description}
-                  </p>
-                </div>
-              </div>
-            </div>
+export default function productDetail(props) {
+  const { id } = props.match.params;
+  const { products } = props;
+  const { img, description, title } = products.find(
+    item => item.id === id.toString()
+  );
+  return (
+    <>
+      <Header />
+      <div className="container">
+        <h4>PRODUCT DETAIL</h4>
+        <div className="row">
+          <div className="col-sm-5">
+            <img src={img} alt="Img" width="100%" />
           </div>
-        ) : (
-          <IsLoading />
-        )}
-      </>
-    );
-  }
+          <div className="col-sm-7">
+            <p>
+              <b>Name:</b> {title}
+            </p>
+            <p>
+              <b>Description:</b> {description}
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
